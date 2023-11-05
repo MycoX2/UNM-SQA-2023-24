145a3cde7ac25f2f12918492275b1bd0676551aa
xhLoh <pabloloh1111@gmail.com>
2023-11-05 15:58:29 +0800

    fix- self addded buttons can be selected in multiple already

--- ytsearch.js
+++ ytsearch.js
@@ -1,25 +1,42 @@
 $(document).ready(function(){
-    var API_KEY = "AIzaSyAYUJirQE2io-hjqAce9IJCQw9D-F6aFNk";   
-    var selectedCategories = [];
+    var API_KEY = "AIzaSyAYUJirQE2io-hjqAce9IJCQw9D-F6aFNk";
+    var selectedCategories = new Set();
 
-    $(".category-button").click(function () {
-        $(this).toggleClass("selected");
+    // Function to handle category button click
+    function handleCategoryClick() {
         var categoryKeyword = $(this).data("category");
-        var $searchInput = $("#search");
-        
-        if ($(this).hasClass("selected")) {
-           selectedCategories.push(categoryKeyword);
+
+        if (selectedCategories.has(categoryKeyword)) {
+            selectedCategories.delete(categoryKeyword);
         } else {
-            selectedCategories = selectedCategories.filter(item => item !== categoryKeyword);
+            selectedCategories.add(categoryKeyword);
         }
 
-        var combinedCategories = selectedCategories.join(" ");
-        if (combinedCategories !== "") {
-            $searchInput.val(combinedCategories);
-        } else {
-            $searchInput.val("");
-        }
-    });
+        $(this).toggleClass("selected");
+        updateSearchInput();
+    }
+
+    // Load predefined categories
+    $(".category-button").on("click", handleCategoryClick);
+
+    // Load custom keywords from local storage
+    var customKeywords = localStorage.getItem("customKeywords");
+    if (customKeywords) {
+        customKeywords = JSON.parse(customKeywords);
+        customKeywords.forEach(function(keyword) {
+            addCustomCategoryButton(keyword);
+            if (selectedCategories.has(keyword)) {
+                $(".category-button[data-category='" + keyword + "']").addClass("selected");
+            }
+        });
+    }
+
+    // Function to add custom category button
+    function addCustomCategoryButton(keyword) {
+        var newCategoryButton = $("<button class='category-button' data-category='" + keyword + "'>" + keyword + "</button>");
+        newCategoryButton.on("click", handleCategoryClick);
+        $(".category-buttons").append(newCategoryButton);
+    }
 
     $("#search-button").click(function () {
         var search = $("#search").val();
@@ -27,91 +44,107 @@ $(document).ready(function(){
         videoSearch(API_KEY, search, 12, videoDuration);
     });
 
-    function videoSearch(key, search, maxResults, videoDuration) {
-        $("#videos").empty();
-        $.get("https://www.googleapis.com/youtube/v3/search?key=" + key
-        + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search + "&videoDuration=" + videoDuration, 
-        function(data){
-            var videosContainer = $("<div class='row'></div>");
-            console.log(data);
-            data.items.forEach(function (item, index) {
-                var videoColumn = $("<div class='col-md-4'></div>");
-                var videoContainer = $("<div class='video-container'></div>");
-        
-                // Create an image or link to represent the video thumbnail and trigger the pop-up
-                var thumbnail = $("<img src='" + item.snippet.thumbnails.medium.url + "' alt='Video Thumbnail'>");
-                thumbnail.on("click", function () {
-                    openVideo("https://www.youtube.com/embed/" + item.id.videoId);
+
+        function videoSearch(key, search, maxResults, videoDuration) {
+            $("#videos").empty();
+            $.get("https://www.googleapis.com/youtube/v3/search?key=" + key
+            + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search + "&videoDuration=" + videoDuration, 
+            function(data){
+                var videosContainer = $("<div class='row'></div>");
+                console.log(data);
+                data.items.forEach(function (item, index) {
+                    var videoColumn = $("<div class='col-md-4'></div>");
+                    var videoContainer = $("<div class='video-container'></div>");
+            
+                    var thumbnail = $("<img src='" + item.snippet.thumbnails.medium.url + "' alt='Video Thumbnail'>");
+                    thumbnail.on("click", function () {
+                        openVideo("https://www.youtube.com/embed/" + item.id.videoId);
+                    });
+            
+                    var videoTitle = $("<p class='video-title'></p>");
+                    videoTitle.text(item.snippet.title);
+            
+                    videoContainer.append(thumbnail);
+                    videoContainer.append(videoTitle);
+                    videoColumn.append(videoContainer);
+                    videosContainer.append(videoColumn);
+            
+                    if ((index + 1) % 3 === 0) {
+                        $("#videos").append(videosContainer);
+                        videosContainer = $("<div class='row'></div>");
+                    }
                 });
-        
-                var videoTitle = $("<p class='video-title'></p>");
-                videoTitle.text(item.snippet.title);
-        
-                videoContainer.append(thumbnail);
-                videoContainer.append(videoTitle);
-                videoColumn.append(videoContainer);
-                videosContainer.append(videoColumn);
-        
-                if ((index + 1) % 3 === 0) {
+
+                if (videosContainer.children().length > 0){
                     $("#videos").append(videosContainer);
-                    videosContainer = $("<div class='row'></div>");
                 }
-            });
+            })
+        }
 
-            if (videosContainer.children().length > 0){
-                $("#videos").append(videosContainer);
+        var videoScreen = document.getElementById("videoScreen");
+        var videoFrame = document.getElementById("videoFrame");
+
+        function openVideo(videoUrl) {
+            videoFrame.src = videoUrl;
+            videoScreen.style.display = "block";
+        }
+
+        function closeVideo() {
+            videoFrame.src = "";
+            videoScreen.style.display = "none";
+        }
+
+        var closeBtn = document.querySelector(".close");
+        closeBtn.addEventListener("click", closeVideo);
+        
+        window.onclick = function(event) {
+            if (event.target === videoScreen) {
+                closeVideo();
             }
-        })
-    }
-
-    var videoScreen = document.getElementById("videoScreen");
-    var videoFrame = document.getElementById("videoFrame");
-
-    function openVideo(videoUrl) {
-        videoFrame.src = videoUrl;
-        videoScreen.style.display = "block";
-    }
-
-    function closeVideo() {
-        videoFrame.src = "";
-        videoScreen.style.display = "none";
-    }
-
-    var closeBtn = document.querySelector(".close");
-    closeBtn.addEventListener("click", closeVideo);
-    
-    window.onclick = function(event) {
-        if (event.target === videoScreen) {
-            closeVideo();
         }
-    }
 
-    $("#addKeywordButton").click(function (){
-        $("#keywordPopup").css("display", "block");
-    });
+        // + button function
+        $("#addKeywordButton").click(function (){
+            console.log("Add Custom Keyword clicked");
+            $("#keywordPopup").css("display", "block");
+        });
 
-    $("#cancelKeyword").click(function (){
-        $("#keywordPopup").css("display", "none");
-    });
-
-    $("#submitKeyword").click(function () {
-        var customKeyword = $("#customKeyword").val();
-        if (customKeyword.trim() !== "") {
-            var newCategoryButton = $("<button class='category-button' data-category='" + customKeyword + "'>" + customKeyword + "</button>");
-            newCategoryButton.click(function () {
-                $(this).toggleClass("selected");
-                var $searchInput = $("#search");
-                if ($(this).hasClass("selected")) {
-                    $searchInput.val(customKeyword);
-                } else {
-                    $searchInput.val("");
-                }
-            });
-
-            $(".category-buttons").append(newCategoryButton);
-            $("#customKeyword").val("");
+        $("#cancelKeyword").click(function (){
             $("#keywordPopup").css("display", "none");
-        }
-    })
-});
+        });
 
+        $("#submitKeyword").click(function () {
+            var customKeyword = $("#customKeyword").val();
+            if (customKeyword.trim() !== "") {
+                var customKeywords = localStorage.getItem("customKeywords");
+                if (customKeywords) {
+                    customKeywords = JSON.parse(customKeywords);
+                    customKeywords.push(customKeyword);
+                } else {
+                    customKeywords = [customKeyword];
+                }
+                localStorage.setItem("customKeywords", JSON.stringify(customKeywords));
+    
+                addCustomCategoryButton(customKeyword);
+                updateSearchInput();
+    
+                // Clear the input and close the popup
+                $("#customKeyword").val("");
+                $("#keywordPopup").css("display", "none");
+            }
+        });
+    
+        // Function to update the search input
+        function updateSearchInput() {
+            var combinedCategories = Array.from(selectedCategories).join(" ");
+            var $searchInput = $("#search");
+            $searchInput.val(combinedCategories);
+        }
+        
+        $("#clearAllKeywords").click(function () {
+            localStorage.removeItem("customKeywords");
+            $(".custom-category-button").remove(); // Remove custom buttons from the DOM
+            selectedCategories.clear(); // Clear the selectedCategories set
+            updateSearchInput();
+        });
+    });
